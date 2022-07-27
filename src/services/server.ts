76f59a9a9b890e8ba486985e0e143a2c57057166
Fabricio  Mores <63478471+FMores@users.noreply.github.com>
2022-07-26 21:02:17 -0300

    agrego middleware cors

--- src/services/server.ts
+++ src/services/server.ts
@@ -10,7 +10,7 @@ import session from 'express-session';
 import config from '../config';
 import { Server } from 'http';
 import path from 'path';
-import mime from 'mime';
+import cors from 'cors';
 
 export const app = express();
 
@@ -24,8 +24,8 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.resolve(__dirname, '../../views'));
 
+app.use(cors());
 app.use(compression());
-
 app.use(express.static(path.resolve(__dirname, '../../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
